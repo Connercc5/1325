60c6a4939ac947e61939981ed55e5c9ff0a02c45
Tolulope0 <44985202+Tolulope0@users.noreply.github.com>
2018-11-19 17:38:06 -0600

    Update ingredient.h

--- Project/ingredient.h
+++ Project/ingredient.h
@@ -9,7 +9,7 @@ class ingredient
         public:
         string name;//name of the ingredient
 	ingredient(double amount,string unit,string ingre_name);
-	int quantity;//amount of each ingredent
+	double quantity;//amount of each ingredent
         string unit;
 	void add_ingredient();
         void modify_ingredient();
