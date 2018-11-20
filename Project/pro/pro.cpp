0e6ee4cd6ce387edba66453f1fbd043ac8e575dd
Connercc5 <Connercc5@users.noreply.github.com>
2018-11-19 20:14:03 -0600

    Merged pro.cpp and pro.hpp
    added code functionality of above files to the actual pro.cpp and pro.h

--- Project/pro/pro.cpp
+++ Project/pro/pro.cpp
@@ -19,22 +19,55 @@ using namespace std;
 Menuwindow::Menuwindow():box(Gtk::ORIENTATION_VERTICAL)
 ,case1("Modify Recipes"),case2("Create Meal Plan",2),case3("Choose Old Shopping list"),case4("Exit",3)
 {
-        set_size_request(400,200);
-        set_title("Preppy Meal");
-        add(box);
-        Gtk::Label label=Gtk::Label();
-        label.set_text("Pick a option");
-	label.set_padding(10,10);
-        box.pack_start(label);
-        box.pack_start(case1);
-        box.pack_start(case2);
-        box.pack_start(case3);
-        box.pack_start(case4);
-        show_all_children();
-
-
+    set_size_request(400,200);
+    set_title("Preppy Meal");
+    add(box);
+    Gtk::Label label=Gtk::Label();
+    label.set_text("Pick a option");
+    label.set_padding(10,10);
+    box.pack_start(label);
+    box.pack_start(case1);
+    box.pack_start(case2);
+    box.pack_start(case3);
+    box.pack_start(case4);
+    show_all_children();
+    
+    
+    case1.signal_clicked().connect(sigc::mem_fun(*this, &Menuwindow::case1_clicked));
+    box.pack_start(case1);
+    
 }
 Menuwindow::~Menuwindow()
 {
-	hide();
 }
+
+
+void Menuwindow::case1_clicked() {
+    hide ();
+    Modify_window w;
+    Gtk::Main::run(w);
+}
+
+Modify_window::Modify_window():box(Gtk::ORIENTATION_VERTICAL), nationality_button("Enter") {
+    set_size_request(400,200);
+    set_title("Modify a recipe");
+    add(box);
+    
+    nationality_label.set_text ("Enter a nationality: ");
+    box.pack_start (nationality_label);
+    
+    nationality_entry.set_max_length (50);
+    nationality_entry.set_text("Enter nationality");
+    nationality_entry.select_region(0, nationality_entry.get_text_length());
+    box.pack_start(nationality_entry);
+    
+    nationality_button.signal_clicked().connect(sigc::mem_fun(*this, &Modify_window::Nationality));
+    box.pack_start(nationality_button);
+    
+     show_all_children();
+    
+}
+
+
+void Modify_window::Nationality() {}
+Modify_window::~Modify_window(){}
--- Project/pro/pro.h
+++ Project/pro/pro.h
@@ -8,23 +8,53 @@ using namespace std;
 
 class Menuwindow: public Gtk::Window
 {
+    
+public:
+    Menuwindow();
+    virtual ~Menuwindow();
+protected:
+    void case1_clicked();
+    void case2_clicked();
+    void case3_clicked();
+    void case4_clicked();
+    
+    
+    Gtk::Button case1;
+    Gtk::Button case2;
+    Gtk::Button case3;
+    Gtk::Button case4;
+    Gtk::Box box;
+private:
+    
+};
 
-        public:
-                Menuwindow();
-                virtual ~Menuwindow();
-        protected:
-                void case1_clicked();
-                void case2_clicked();
-                void case3_clicked();
-                void case4_clicked();
+
+class Modify_window: public Gtk::Window{
+public:
+    Modify_window ();
+    virtual ~Modify_window();
+    
+
+protected:
+    
+    void Nationality ();
+    
+    Gtk::Label nationality_label;
+    Gtk::Entry nationality_entry;
+    Gtk::Button nationality_button;
+     Gtk::Box box;
+    
+};
 
 
-                Gtk::Button case1;
-                Gtk::Button case2;
-                Gtk::Button case3;
-                Gtk::Button case4;
-                Gtk::Box box;
-        private:
+class Recipe_Info {
+public:
+    string food_type, recipe_name, recipe;
+    Recipe_Info (string food_type_p, string recipe_name_p, string recipe_p);
+};
 
+class Database {
+    
 };
 #endif
+
